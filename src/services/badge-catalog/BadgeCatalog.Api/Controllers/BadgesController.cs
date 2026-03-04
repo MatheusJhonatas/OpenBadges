cb800a87195b69381b86503d63b0935cb526208c
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-04 18:42:47 -0300

    add controller

--- src/services/badge-catalog/BadgeCatalog.Api/Controllers/BadgesController.cs
+++ src/services/badge-catalog/BadgeCatalog.Api/Controllers/BadgesController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Specialized;
+using BadgeCatalog.Application.Commands.CreateBadgeClass;
+using BadgeCatalog.Application.Queries.GetAllBadges;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BadgeCatalog.Api.Controllers;
@@ -6,4 +9,16 @@ namespace BadgeCatalog.Api.Controllers;
 [Route("api/[controller]")]
 public class BadgesController : ControllerBase
 {
+    private readonly CreateBadgeClassHandler _createBadgeClass;
+    public BadgesController(CreateBadgeClassHandler createBadgeClass)
+    {
+        _createBadgeClass = createBadgeClass;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBadgeClass(CreateBadgeClassCommand command, CancellationToken cancellationToken)
+    {
+        var id = await _createBadgeClass.Handle(command, cancellationToken);
+        return Created($"/api/badges/{id}", new { Id = id });
+    }
 }
