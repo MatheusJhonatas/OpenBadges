2c8301bce0c315a5843e93218963fd6913732bdd
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-11 17:17:06 -0300

    Add Scalar

--- src/services/issuance/Issuance.Api/Program.cs
+++ src/services/issuance/Issuance.Api/Program.cs
@@ -1,37 +1,29 @@
+using Issuance.Application.Commands.IssueBadge;
+using MediatR;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(IssueBadgeCommand).Assembly));
 
 var app = builder.Build();
 
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
+app.MapOpenApi();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/scalar", () => Results.Redirect("/scalar/v1"));
+app.MapGet("/scalar/", () => Results.Redirect("/scalar/v1"));
+app.MapScalarApiReference(options =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    options.AddDocument("v1");
+    options.WithTitle("Issuance API");
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
