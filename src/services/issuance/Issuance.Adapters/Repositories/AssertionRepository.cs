ba79c467918b2c7a578af2f963dd87fd876443ac
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-12 12:15:50 -0300

    add AssertionRepository

---
+++ src/services/issuance/Issuance.Adapters/Repositories/AssertionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Issuance.Domain.Aggregates;
+using Issuance.Ports.Repositories;
+
+namespace Issuance.Adapters.Repositories;
+
+
+public class AssertionRepository : IAssertionRepository
+{
+    public Task AddAsync(Assertion assertion, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
+
