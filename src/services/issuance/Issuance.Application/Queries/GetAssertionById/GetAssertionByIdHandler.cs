02cf50b76b25d904860a1f614ad384d659ca8245
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-19 18:36:36 -0300

    Update AssertionQuery and AssertionHandler

--- src/services/issuance/Issuance.Application/Queries/GetAssertionById/GetAssertionByIdHandler.cs
+++ src/services/issuance/Issuance.Application/Queries/GetAssertionById/GetAssertionByIdHandler.cs
@@ -1,19 +1,29 @@
+using Issuance.Application.Dtos;
 using Issuance.Domain.Aggregates;
 using Issuance.Ports.Repositories;
 using MediatR;
 
 namespace Issuance.Application.Queries.GetAssertionById;
 
-public class GetAssertionByIdHandler : IRequestHandler<GetAssertionByIdQuery, Assertion?>
+public class GetAssertionByIdHandler : IRequestHandler<GetAssertionByIdQuery, AssertionResponse?>
 {
     private readonly IAssertionRepository _repository;
     public GetAssertionByIdHandler(IAssertionRepository repository)
     {
         _repository = repository;
     }
-    public async Task<Assertion?> Handle(GetAssertionByIdQuery request, CancellationToken cancellationToken)
+    public async Task<AssertionResponse?> Handle(GetAssertionByIdQuery request, CancellationToken cancellationToken)
     {
        var assertion = await _repository.GetByIdAsync(request.Id, cancellationToken);
-       return assertion;
+       if (assertion == null) return null;
+
+       return new AssertionResponse
+       {
+           Id = assertion.Id,
+            BadgeClassId = assertion.BadgeClassId,
+            HashedEmail = assertion.Recipient.HashedEmail,
+            IssuedOn = assertion.IssuedOn,
+            Status = (int)assertion.Status
+       };
     }
 }
--- src/services/issuance/Issuance.Application/Queries/GetAssertionById/GetAssertionByIdQuery.cs
+++ src/services/issuance/Issuance.Application/Queries/GetAssertionById/GetAssertionByIdQuery.cs
@@ -1,7 +1,8 @@
 using MediatR;
 using Issuance.Domain.Aggregates;
+using Issuance.Application.Dtos;
 
 namespace Issuance.Application.Queries.GetAssertionById;
 
 // Query que representa a intenção de buscar uma assertion por ID
-public record GetAssertionByIdQuery(Guid Id) : IRequest<Assertion?>;
+public record GetAssertionByIdQuery(Guid Id) : IRequest<AssertionResponse?>;
