ba4539ae5d82573b58c085f06b1cecc8ed2a3a43
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-24 19:36:42 -0300

    Multiplos handlers funcionando

--- src/services/issuance/Issuance.Application/EventsHandlers/BadgeIssued/BadgeIssuedEventHandler.cs
+++ src/services/issuance/Issuance.Application/EventsHandlers/BadgeIssued/BadgeIssuedEventHandler.cs
@@ -1,15 +1,21 @@
 using Issuance.Domain.Events;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Issuance.Application.EventsHandlers.BadgeIssued;
 
 // Handler que reage ao evento de badge emitido
 public class BadgeIssuedEventHandler : INotificationHandler<BadgeIssuedEvent>
 {
+    private readonly ILogger<BadgeIssuedEventHandler> _logger;
+    public BadgeIssuedEventHandler(ILogger<BadgeIssuedEventHandler> logger)
+    {
+        _logger = logger;
+    }
     public Task Handle(BadgeIssuedEvent notification, CancellationToken cancellationToken)
     {
         // 🔥 Simulação de ação (ex: log, email, integração)
-        Console.WriteLine($"[EVENT] Badge issued for {notification.HashedEmail }");
+        _logger.LogInformation("Handler ORIGINAL executado");
 
         return Task.CompletedTask;
     }
--- src/services/issuance/Issuance.Application/EventsHandlers/BadgeIssued/SendBadgeIssuedEmailHandler.cs
+++ src/services/issuance/Issuance.Application/EventsHandlers/BadgeIssued/SendBadgeIssuedEmailHandler.cs
@@ -1,12 +1,22 @@
 using Issuance.Domain.Events;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Issuance.Application.EventsHandlers.BadgeIssued;
 
 public class SendBadgeIssuedEmailHandler : INotificationHandler<BadgeIssuedEvent>
 {
+    private readonly ILogger<SendBadgeIssuedEmailHandler> _logger;
+
+    public SendBadgeIssuedEmailHandler(ILogger<SendBadgeIssuedEmailHandler> logger)
+    {
+        _logger = logger;
+    }
     public Task Handle(BadgeIssuedEvent notification, CancellationToken cancellationToken)
     {
+        // Simulação de envio de email
+        _logger.LogInformation("Email handler executado");
+
         return Task.CompletedTask;
     }
 }
