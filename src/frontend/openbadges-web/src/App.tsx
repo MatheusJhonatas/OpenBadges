20cebcab7e13536c1529c16a20f46fdd1b77b489
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-03-25 18:50:57 -0300

    add route

--- src/frontend/openbadges-web/src/App.tsx
+++ src/frontend/openbadges-web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar";
 import { HomePage } from "./pages/HomePage";
+import { CatalogPage } from "./pages/CatalogPage";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<div>Dashboard</div>} />
         <Route path="/meus-badges" element={<div>Meus Badges</div>} />
-        <Route path="/admin/catalogo" element={<div>Admin: Catálogo</div>} />
+        <Route path="/admin/catalogo" element={<CatalogPage />} />
         <Route path="/admin/emitir" element={<div>Admin: Emitir</div>} />
         <Route path="/admin/revogar" element={<div>Admin: Revogar</div>} />
       </Routes>
