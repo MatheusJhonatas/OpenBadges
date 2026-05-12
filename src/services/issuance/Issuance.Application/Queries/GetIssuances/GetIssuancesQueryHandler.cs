38e8ae0872d116a666474a61b4316a5526a8471f
mjhonata <129222113+MatheusJhonatas@users.noreply.github.com>
2026-05-11 21:17:26 -0300

    develop

---
+++ src/services/issuance/Issuance.Application/Queries/GetIssuances/GetIssuancesQueryHandler.cs
@@ -0,0 +1,38 @@
+using Issuance.Application.Dtos;
+using Issuance.Ports.Repositories;
+using MediatR;
+
+namespace Issuance.Application.Queries.GetIssuances;
+
+public class GetIssuancesQueryHandler : IRequestHandler<GetIssuancesQuery, List<GetIssuancesResponse>>
+{
+    private readonly IAssertionRepository _assertionRepository;
+
+    public GetIssuancesQueryHandler(IAssertionRepository assertionRepository)
+    {
+        _assertionRepository = assertionRepository;
+    }
+
+    public async Task<List<GetIssuancesResponse>> Handle(GetIssuancesQuery request, CancellationToken cancellationToken)
+    {
+        var assertions = await _assertionRepository.GetAllAsync(cancellationToken);
+
+        return assertions.Select(x => new GetIssuancesResponse
+        {
+            Id = x.Id,
+
+                RecipientName = x.RecipientName,
+
+                RecipientHashedEmail =
+                    x.Recipient.HashedEmail,
+
+                BadgeClassId = x.BadgeClassId,
+
+                Status = x.Status.ToString(),
+
+                IssuedOn = x.IssuedOn,
+
+                RevokedOn = x.RevokedOn
+        }).ToList();
+    }
+}
